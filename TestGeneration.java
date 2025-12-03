053ea94e8ebc67ce6044a8a7c966592e341c27c0
Uch19 <145311804+Uch19@users.noreply.github.com>
2025-12-03 16:49:13 -0500

    Add files via upload

---
+++ TestGeneration.java
@@ -0,0 +1,24 @@
+package binary;
+
+import java.util.*;
+
+public class TestGeneration {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String filename = scanner.nextLine();
+
+    Generation gen = new Generation(500,5,filename);
+
+    gen.printBesTree();
+    gen.printBestFitness();
+
+    System.out.println("\nTop Ten Fitness Values;");
+    for(GPTree tree : gen.getTopTen()) {
+      System.out.println(tree.getFitness());
+    }
+    scanner.close();
+ }
+} 
+
