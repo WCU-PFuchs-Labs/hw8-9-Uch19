38a3840b96b89db85d56b01e8e5e4d0696d2746f
Uch19 <145311804+Uch19@users.noreply.github.com>
2025-12-03 21:05:05 -0500

    Add constructor to Binop class for left and right nodes

--- binary/Binop.java
+++ binary/Binop.java
@@ -6,5 +6,15 @@ package binary;
  * Purpose: A base class for binary arithmetic operations
  */
 public abstract class Binop extends Op {
+     
+     protected Node left;
+     protected Node right;
+
+     public Binop(Node left, Node right) {
+         this.left = left;
+         this.right = right;
+     }
+     
+     
      public abstract double eval(double left, double right);
 }
