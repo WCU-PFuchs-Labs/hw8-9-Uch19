2fcb2f1e558fee7a36eed79e8b2c6ec346d2d428
Uch19 <145311804+Uch19@users.noreply.github.com>
2025-12-10 12:32:13 -0500

    Create TestGP.java

---
+++ TestGP.java
@@ -0,0 +1,29 @@
+import binary.*;
+
+import java.util.*;
+
+public class TestGeneration {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String filename = scanner.nextLine();
+
+    Generation gen = new Generation(500,5,filename);
+
+    for(int i = 0; i < 50; i++)
+      {
+        System.out.println("\nGeneration" + i + ":");
+
+      gen.printBestTree();
+      gen.printBestFitness();
+
+      gen.evolve();
+        
+      }
+
+     
+  
+  }
+  scanner.close();
+}  
